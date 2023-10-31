68329f0052428793f12428fb0c222c3f6d445752
Raj Rana <raj.rana@fiserv.com>
2023-10-31 14:12:12 -0400

    solved the Algo Hour

--- Solution.java
+++ Solution.java
@@ -1,12 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public static char mostFrequentChar(String s) {
-      
-      return;
+
+        int max = Integer.MIN_VALUE;
+        HashMap<Character, Integer> frequency = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(frequency.containsKey(s.charAt(i))){
+                frequency.put(s.charAt(i), frequency.get(s.charAt(i))+1);
+                if(frequency.get(s.charAt(i)) >= max){
+                    max = frequency.get(s.charAt(i));
+                }
+            } else {
+                frequency.put(s.charAt(i), 1);
+            }
+
+        }
+
+
+        for (Map.Entry x : frequency.entrySet()) {
+            if((int)x.getValue() == max)
+                return (char)x.getKey();
+        }
+        return 0;
     }
   
     public static void main(String[] args) {
       // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("bookeepeoooor")); // -> 'e'
     }
 
   }
